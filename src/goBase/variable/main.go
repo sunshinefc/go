73753c34587f7796773e4b443ae0967121ca5ddf
feicongcong <1075025698@qq.com>
2019-05-06 11:01:14 +0800

    '变量声明赋值'

---
+++ src/goBase/variable/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var a int
+var b int=456
+var (
+	c string ="历史"
+	d int =1994
+)
+func main()  {
+	a=123
+
+	fmt.Print(a)
+	fmt.Print("\n")
+
+	fmt.Print(b)
+	fmt.Print("\n")
+
+	fmt.Print(c)
+	fmt.Print("\n")
+
+	fmt.Print(d)
+}
