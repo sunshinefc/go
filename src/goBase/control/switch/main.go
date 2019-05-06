f50a1ce89153f7bc07295cb709a1bb089aed16f3
feicongcong <1075025698@qq.com>
2019-05-06 14:52:19 +0800

    'switch'

--- src/goBase/control/switch/main.go
+++ src/goBase/control/switch/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main()  {
-	
+	switch 3 {
+	case 1:
+		fmt.Print("判断为1\n")
+	case 2:
+		fmt.Print("判断为2\n")
+	default:
+		fmt.Print("以上都不满足\n")
+	}
+
+
+	var a interface{}
+	a="history"
+	switch a.(type) {
+	case int:
+		fmt.Print("类型为整型")
+	case string:
+		fmt.Print("类型为字符串类型")
+	default:
+		fmt.Print("以上都不是")
+	}
 }
