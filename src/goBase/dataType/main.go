7154baefa4030d44f9bff4e6778edf16acef01e7
feicongcong <1075025698@qq.com>
2019-05-06 10:19:41 +0800

    'datatype'

--- src/goBase/dataType/main.go
+++ src/goBase/dataType/main.go
@@ -8,21 +8,27 @@ import (
 func main()  {
 	//无符号整型
 	var a uint8 =1
+
+	//整型
 	var b int32 =1
 	var c int =1
 	fmt.Print(unsafe.Sizeof(a))
 	fmt.Print(unsafe.Sizeof(b))
 	fmt.Print(unsafe.Sizeof(c))
 
+	//浮点型
 	var x float32=1.2
 	fmt.Print(unsafe.Sizeof(x))
 
+	//布尔
 	var t bool=true
 	fmt.Print(t)
 
+	//字节
 	var i byte=1
 	fmt.Print(unsafe.Sizeof(i))
 
 	var j rune=1
 	fmt.Print(unsafe.Sizeof(j))
+
 }
