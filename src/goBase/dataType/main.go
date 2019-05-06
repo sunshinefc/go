9b9001c2d4aa4f788cad8bf2415b12687567790c
feicongcong <1075025698@qq.com>
2019-05-06 10:09:47 +0800

    'datatype'

--- src/goBase/dataType/main.go
+++ src/goBase/dataType/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"unsafe"
+)
+
 func main()  {
-	
+	//无符号整型
+	var a uint8 =1
+	var b int32 =1
+	var c int =1
+	fmt.Print(unsafe.Sizeof(a))
+	fmt.Print(unsafe.Sizeof(b))
+	fmt.Print(unsafe.Sizeof(c))
+
+	var x float32=1.2
+	fmt.Print(unsafe.Sizeof(x))
+
+	var t bool=true
+	fmt.Print(t)
+
+	var i byte=1
+	fmt.Print(unsafe.Sizeof(i))
+
+	var j rune=1
+	fmt.Print(unsafe.Sizeof(j))
 }
