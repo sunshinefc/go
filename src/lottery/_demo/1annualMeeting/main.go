e7d078ccbe5c327152b2346a033f044c9f9ec2c9
feicongcong <1075025698@qq.com>
2019-05-07 17:04:15 +0800

    'demo'

--- src/lottery/_demo/1annualMeeting/main.go
+++ src/lottery/_demo/1annualMeeting/main.go
@@ -1,6 +1,18 @@
+/*
+curl http://localhost:8080/
+curl --data "users=yifan,yifan2" http://localhost:8080/import
+curl http://localhost:8080/lucky
+
+*/
 package main
 
-import "github.com/kataras/iris"
+import (
+	"fmt"
+	"github.com/kataras/iris"
+	"math/rand"
+	"strings"
+	"time"
+)
 
 var userList []string
 
@@ -42,5 +54,22 @@ func (c*lotteryController) PostImport() string {
 	count2 :=len(userList)
 	return fmt.Sprintf("当前总共参与抽奖的用户数： %d，成功导入的用户数： %d/n",
 		count2,(count2-count1))
+}
+//GET http://localhost:8080/lucky
+func (c *lotteryController) GetLucky() string  {
+	count :=len(userList)
+	if count>1{
+		seed :=time.Now().UnixNano()
+		index :=rand.New(rand.NewSource(seed)).Int31n(int32(count))
+		user :=userList[index]
+		userList=append(userList[0:index],userList[index+1:]...)
+		return fmt.Sprintf("当前中奖用户:%s,剩余用户数:%d\n",user,count-1)
+	}else if count==1{
+		user :=userList[0]
+		return fmt.Sprintf("当前中奖用户:%s,剩余用户数:%d\n",user,count-1)
+	}else{
+		return fmt.Sprintf("已经没有参与用户，请先通过 /import 导入用户\n",)
+	}
+
 }
 
